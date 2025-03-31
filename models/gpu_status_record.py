c0601edb7a0e0661a3f98848c205fda906d9ef45
Aidan Juma <aidan@aidanjuma.dev>
2025-03-31 23:40:31 +0100

    🚧 EoD commit - data modelling...

--- models/gpu_status_record.py
+++ models/gpu_status_record.py
@@ -6,15 +6,26 @@ from models.gpu_info import GPUInfo
 # TODO: Continue...
 
 class GPUStatusRecord(GPUInfo):
-    def __init__(self, name: str, bus_id: str, vram_capacity_mib: int, memory_free_mib: int, memory_used_mib: int):
+    def __init__(self, name: str, bus_id: str, vram_capacity_mib: int, timestamp: str, p_state: str, temperature: int,
+                 memory_free_mib: int,
+                 memory_used_mib: int):
         super().__init__(name, bus_id, vram_capacity_mib)
-
+        self.timestamp = timestamp
+        self.p_state = p_state  # P0 (max. pwr.) - P12 (min. pwr.)
+        self.temperature = temperature  # in deg. C.
         self.memory_free_mib = memory_free_mib
         self.memory_used_mib = memory_used_mib
 
 
 class GPUStatusRecordSchema(Schema):
+    # Inherited:
     name = fields.String(required=True)
     bus_id = fields.String(required=False, default=None)
     vram_capacity_mib = fields.Integer(required=True)
+
+    # New:
+    timestamp = fields.DateTime(required=True)
+    p_state = fields.String(required=True)
+    temperature = fields.Integer(required=True)
+    memory_free_mib = fields.Integer(required=True)
     memory_used_mib = fields.Integer(required=True)
